e089664769d1ae7173872f9c92f2f263914f6803
kminjoo9093 <kminjoo9093@naver.com>
2026-04-03 15:51:25 +0900

    tanstack query 데이터 요청 관리

---
+++ onebite-exam/src/api/fetch-todos.ts
@@ -0,0 +1,10 @@
+import { SERVER_URL } from "@/lib/constants";
+import type { Todo } from "@/types";
+
+export const fetchTodos = async () => {
+  const response = await fetch(`${SERVER_URL}/todos`);
+  if(!response.ok) throw new Error("Fetch Failed");
+
+  const data: Todo[] = await response.json();
+  return data;
+}
