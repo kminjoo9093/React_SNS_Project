e3994ed055d16cf1a6eb439280e6ada0135fce15
kminjoo9093 <kminjoo9093@naver.com>
2026-05-16 15:08:48 +0900

    회원가입 구현

--- onebite-log/src/pages/sign-up-page.tsx
+++ onebite-log/src/pages/sign-up-page.tsx
@@ -1,3 +1,51 @@
-export default function signUpPage() {
-  return <div>signUpPage</div>;
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useSignUp } from "@/hooks/mutations/use-sign-up";
+import { useState } from "react";
+import { Link } from "react-router";
+
+export default function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { mutate: signUp } = useSignUp();
+
+  const handleSignUpClick = () => {
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+
+    signUp({ email, password });
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="text-xl font-bold">회원가입</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="py-6"
+          type="email"
+          placeholder="example@abc.com"
+        />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="py-6"
+          type="password"
+          placeholder="password"
+        />
+      </div>
+      <div>
+        <Button onClick={handleSignUpClick} className="w-full">
+          회원가입
+        </Button>
+      </div>
+      <div>
+        <Link to={"/sign-in"} className="text-muted-foreground hover:underline">
+          이미 계정이 있다면? 로그인
+        </Link>
+      </div>
+    </div>
+  );
 }
