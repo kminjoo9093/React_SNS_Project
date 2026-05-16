7737517655f990d7310c78c8bb899bdbd5812b17
kminjoo9093 <kminjoo9093@naver.com>
2026-05-16 17:03:36 +0900

    로그인 구현

--- onebite-log/src/api/auth.ts
+++ onebite-log/src/api/auth.ts
@@ -7,8 +7,22 @@ export async function signUp({
   email: string;
   password: string;
 }) {
-  const {data, error} = await supabase.auth.signUp({email, password});
+  const { data, error } = await supabase.auth.signUp({ email, password });
 
+  if (error) throw error;
+  return data;
+}
+
+export async function signInWithPassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const {data, error} = await supabase.auth.signInWithPassword({email, password});
+  
   if(error) throw error;
+
   return data;
 }
