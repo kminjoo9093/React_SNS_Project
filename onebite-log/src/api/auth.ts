78a81b42dcfadca5b2884de175d7e53bc43140e6
kminjoo9093 <kminjoo9093@naver.com>
2026-05-26 16:24:23 +0900

    feat: 비밀번호 재설정 구현

--- onebite-log/src/api/auth.ts
+++ onebite-log/src/api/auth.ts
@@ -21,17 +21,38 @@ export async function signInWithPassword({
   email: string;
   password: string;
 }) {
-  const {data, error} = await supabase.auth.signInWithPassword({email, password});
-  
-  if(error) throw error;
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw error;
 
   return data;
 }
 
-export async function signInWithOAuth(provider: Provider){
-  const {data, error} = await supabase.auth.signInWithOAuth({
+export async function signInWithOAuth(provider: Provider) {
+  const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
-  })
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+export async function requestPasswordResetEmail(email: string) {
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: `${import.meta.env.VITE_PUBLIC_URL}/reset-password`,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+export async function updatePassword(password: string) {
+  const { data, error } = await supabase.auth.updateUser({
+    password,
+  });
 
   if(error) throw error;
   return data;
