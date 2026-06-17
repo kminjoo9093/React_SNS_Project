a153b0e8a7ac06223aaba01794184f10bf2ba4a9
kminjoo9093 <kminjoo9093@naver.com>
2026-06-17 18:27:50 +0900

    feat: 프로필 수정 UI 구현

--- onebite-log/src/components/profile/profile-info.tsx
+++ onebite-log/src/components/profile/profile-info.tsx
@@ -2,8 +2,13 @@ import { useProfileData } from "@/hooks/queries/use-profile-data";
 import Fallback from "../fallback";
 import Loader from "@/components/loader";
 import defaultAvatar from "@/assets/default-avatar.png";
+import { useSession } from "@/store/session";
+import EditProfileButton from "./edit-profile";
 
 export default function ProfileInfo({ userId }: { userId: string }) {
+
+  const session = useSession();
+
   const {
     data: profile,
     error: fetchProfileError,
@@ -12,6 +17,8 @@ export default function ProfileInfo({ userId }: { userId: string }) {
 
   if (fetchProfileError) return <Fallback />;
   if (isFetcingProfilePending) return <Loader />;
+
+  const isMine = session?.user.id === userId;
 
   return (
     <div className="flex flex-col items-center justify-center gap-5">
@@ -23,6 +30,7 @@ export default function ProfileInfo({ userId }: { userId: string }) {
         <div className="text-xl font-bold">{profile.nickname}</div>
         <div className="text-muted-foreground">{profile.bio}</div>
       </div>
+      {isMine && <EditProfileButton/>}
     </div>
   );
 }
