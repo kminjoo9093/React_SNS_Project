e6f78e2eb71f7d8a99c4c65b725bc9a722f17351
kminjoo9093 <kminjoo9093@naver.com>
2026-06-20 16:53:11 +0900

    feat: 캐시 수정 기능 추가

--- onebite-log/src/hooks/mutations/comment/use-delete-comment.ts
+++ onebite-log/src/hooks/mutations/comment/use-delete-comment.ts
@@ -1,15 +1,28 @@
 import { deleteComment } from "@/api/comment";
-import type { UseMuationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Comment, UseMuationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-export function useDeleteComment(callbacks?: UseMuationCallback){
+export function useDeleteComment(callbacks?: UseMuationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deleteComment,
-    onSuccess: () => {
-      if(callbacks?.onSuccess) callbacks.onSuccess();
+    onSuccess: (deletedComment) => {
+      if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Comment[]>(
+        QUERY_KEYS.comment.post(deletedComment.post_id),
+        (comments) => {
+          if (!comments)
+            throw new Error("댓글이 캐시 데이터에 보관되어 있지 않습니다.");
+
+          return comments.filter((comment) => comment.id !== deletedComment.id);
+        },
+      );
     },
     onError: (error) => {
-      if(callbacks?.onError) callbacks.onError(error);
-    }
-  })
+      if (callbacks?.onError) callbacks.onError(error);
+    },
+  });
 }
